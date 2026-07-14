d307137f75f597ca6b57ce71101709b7d18e87dc
OUBOUSKEN ILYAS <iliasoub15@gmail.com>
2026-07-14 23:55:37 +0100

    Add show_args script to demonstrate handling of script arguments in bash

---
+++ chapter1/show_args.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+echo "the arguments are: $@"
+echo "the number of arguments is: $#"
+
+# special variable available in bash script
+
+
+# "$0"  The name of the script file
+
+
+
+# "$1, $2, $3, ..." Positional arguments
+
+
+
+#  "$#"  The number of passed positional arguments
+
+
+
+# "$*" All positional arguments
+
+
+# "$@" All positional arguments, where each argument is individually quoted
