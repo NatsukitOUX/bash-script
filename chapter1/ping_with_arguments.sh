21b7d0d184dc25233f79732172a4c7a17a8527b0
OUBOUSKEN ILYAS <iliasoub15@gmail.com>
2026-07-14 19:52:52 +0100

    Add ping script to execute pings on provided target addresses

---
+++ chapter1/ping_with_arguments.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# this script will ping any address provided as an argument.
+
+SCRIPT_NAME="${0}"
+TARGET="${1}"
+
+echo "Running the script ${SCRIPT_NAME}..."
+echo "Pinging the target: ${TARGET}..."
+ping "${TARGET}"
+
+
+
+
