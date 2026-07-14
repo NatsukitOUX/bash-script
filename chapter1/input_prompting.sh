c3091263358ac8d8ee0fc75967032524aec0882c
OUBOUSKEN ILYAS <iliasoub15@gmail.com>
2026-07-15 00:41:26 +0100

    Add input_prompting script to collect user first and last names

---
+++ chapter1/input_prompting.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+# Takes input from the user and assigns it to variables
+echo "what is your first name?"
+read -r firstname
+
+echo "what is your last name?"
+read -r lastname
+
+echo "Your first name is ${firstname} and your last name is ${lastname}"
