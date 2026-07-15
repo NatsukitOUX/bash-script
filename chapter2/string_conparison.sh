eef8189e5e50c87f4781517758345b75a0350406
OUBOUSKEN ILYAS <iliasoub15@gmail.com>
2026-07-16 00:51:26 +0100

    Add string_comparison script to demonstrate string comparison in bash

---
+++ chapter2/string_conparison.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+VARIABLE_ONE="nostarch"
+VARIABLE_TWO="nostarch"
+
+if [[ "${VARIABLE_ONE} == ${VARIABLE_TWO}" ]]; then
+        echo "the are equal"
+else
+        echo "the are not equal"
+fi
+
+
