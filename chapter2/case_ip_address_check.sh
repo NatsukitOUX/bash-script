4fa8497ec5c8ebaa9d2288d551c4a6200aaa7a76
OUBOUSKEN ILYAS <iliasoub15@gmail.com>
2026-07-20 01:45:48 +0100

    Add comments to case_ip_address_check.sh for grep usage examples

--- chapter2/case_ip_address_check.sh
+++ chapter2/case_ip_address_check.sh
@@ -12,3 +12,13 @@ case ${IP_ADDRESS} in
     echo "Could not identify the network"
     ;;
 esac
+
+# use grep to filter out information of interest from streams
+# grep "35.237.4.214" log.txt
+
+# use grep for multiple patterns simultaneously or by using the -e flag to achieve the same result
+# grep "35.237.4.214\|13.66.139.0" log.txt 
+# grep -e "35.237.4.214" -e "13.66.139.0" log.txt
+
+# We can also use grep with the -v argument to exclude lines containing a certain pattern
+# grep -v "35.237.4.214" log.txt
