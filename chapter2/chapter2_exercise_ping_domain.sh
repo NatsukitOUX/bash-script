5cce93d983743ca6d5328e93a03ba6a84957805d
Ilyas-Oubousken <ilyasoubousken@gmail.com>
2026-07-22 00:48:12 +0300

    Fix argument check in chapter2_exercise_ping_domain.sh script

--- chapter2/chapter2_exercise_ping_domain.sh
+++ chapter2/chapter2_exercise_ping_domain.sh
@@ -3,7 +3,10 @@
 NAME="${1}"
 TARGET_DOMAIN="${2}"
 
-if [[ -z "${NAME}" && "${TARGET_DOMAIN}" ]]; then
-    echo "Now argument was provided. Exiting..."
+if [[ -z "${NAME}" && -z "${TARGET_DOMAIN}" ]]; then
+
+    echo "No argument was provided. Exiting..."
     exit 1
+    
+fi
 
