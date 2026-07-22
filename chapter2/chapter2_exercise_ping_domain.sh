69420d44923c1c509da16238a0339ac12f5fb8b1
Ilyas-Oubousken <ilyasoubousken@gmail.com>
2026-07-22 03:29:01 +0100

    Fix output formatting and ensure results are logged correctly in chapter2_exercise_ping_domain.sh

--- chapter2/chapter2_exercise_ping_domain.sh
+++ chapter2/chapter2_exercise_ping_domain.sh
@@ -11,15 +11,18 @@ if [[ -z "${NAME}" || -z "${TARGET_DOMAIN}" ]]; then
     exit 1
 fi
 
+if [[ ! -f "${OUTPUT_FILE}" ]]; then
+    echo "Status,Name,Domain,TimeStamp" >> "${OUTPUT_FILE}"
+fi
 
 if ping -c1 ${TARGET_DOMAIN} &> /dev/null; then
     echo "The ping command was seccesfull!"
     status="Success"
-    echo "${DOMAIN},${TARGET_DOMAIN},status,${date}" >> "${OUTPUT_FILE}"
+    echo "${status},${NAME},${TARGET_DOMAIN},$(date +"%Y-%m-%d %H:%M:%S")" >> "${OUTPUT_FILE}"
 else
     echo "The ping command did not run!"
     status="Failure"
-    
+    echo "${status},${NAME},${TARGET_DOMAIN},$(date +"%Y-%m-%d %H:%M:%S")" >> "${OUTPUT_FILE}"
 fi
 
     
