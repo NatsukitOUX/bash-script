f56e3e36bbe0b04a98b30d3419bb6ea1bb8e2760
Ilyas-Oubousken <ilyasoubousken@gmail.com>
2026-07-22 02:23:02 +0100

    Fix argument check logic in chapter2_exercise_ping_domain.sh and clean up ping command handling

--- chapter2/chapter2_exercise_ping_domain.sh
+++ chapter2/chapter2_exercise_ping_domain.sh
@@ -2,20 +2,24 @@
 
 NAME="${1}"
 TARGET_DOMAIN="${2}"
-
+OUTPUT_FILE="results.csv"
 # the following code exit if the user did not provide any arguments
 
-if [[ -z "${NAME}" && -z "${TARGET_DOMAIN}" ]]; then
+if [[ -z "${NAME}" || -z "${TARGET_DOMAIN}" ]]; then
 
     echo "No argument was provided. Exiting..."
     exit 1
 fi
 
-if ping -c1 ${TARGET_DOMAIN}; then
+
+if ping -c1 ${TARGET_DOMAIN} &> /dev/null; then
     echo "The ping command was seccesfull!"
-    awk '{print $0, $NAME , $TARGET_DOMAIN , }'
+    status="Success"
+    echo "${DOMAIN},${TARGET_DOMAIN},status,${date}" >> "${OUTPUT_FILE}"
 else
     echo "The ping command did not run!"
+    status="Failure"
+    
 fi
 
     
