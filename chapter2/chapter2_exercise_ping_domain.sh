5d5c3b2382b13b0d5169739711264fea0744c674
Ilyas-Oubousken <ilyasoubousken@gmail.com>
2026-07-22 01:00:32 +0100

    Refactor argument check and improve ping command output in chapter2_exercise_ping_domain.sh

--- chapter2/chapter2_exercise_ping_domain.sh
+++ chapter2/chapter2_exercise_ping_domain.sh
@@ -3,10 +3,19 @@
 NAME="${1}"
 TARGET_DOMAIN="${2}"
 
+# the following code exit if the user did not provide any arguments
+
 if [[ -z "${NAME}" && -z "${TARGET_DOMAIN}" ]]; then
 
     echo "No argument was provided. Exiting..."
     exit 1
-    
 fi
 
+if ping -c1 ${TARGET_DOMAIN}; then
+    echo "The ping command was seccesfull!"
+    awk '{print $0, $NAME , $TARGET_DOMAIN , }'
+else
+    echo "The ping command did not run!"
+fi
+
+    
