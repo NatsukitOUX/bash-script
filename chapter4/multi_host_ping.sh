da32ea30bfec340fbcebfb866ce35ad4208c709f
OUBOUSKEN ILYAS <iliasoub15@gmail.com>
2026-07-27 00:53:13 +0100

    Add multi_host_ping.sh script to check host availability

---
+++ chapter4/multi_host_ping.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+# FILE="${1}"
+
+# while read -r host; do
+#     if ping -c 1 -W 1 -w 1 "${host}" &> /dev/null; then
+#         echo "${host} is up."
+#     fi
+# done < "${FILE}"
+
+FILE="${1}"
+
+while read -r host; do
+  if ping -c 1 -W 1 -w 1 "${host}" &> /dev/null; then
+    echo "${host} is up."
+  fi
+done < "${FILE}"
