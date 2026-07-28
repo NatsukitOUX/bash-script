09bab9101d66feca59ef82209994e85fbdd0ad6c
OUBOUSKEN ILYAS <iliasoub15@gmail.com>
2026-07-28 03:25:34 +0100

    Refactor multi_host_ping.sh to improve readability and add output for down hosts

--- chapter4/multi_host_ping.sh
+++ chapter4/multi_host_ping.sh
@@ -1,18 +1,11 @@
 #! /bin/bash
-# FILE="${1}"
-
-# while read -r host; do
-#     if ping -c 1 -W 1 -w 1 "${host}" &> /dev/null; then
-#         echo "${host} is up."
-#     fi
-# done < "${FILE}"
-
 FILE="${1}"
 
 while read -r host; do
-  if ping -c 1 -W 1 -w 1 "${host}" &> /dev/null; then
-    echo "${host} is up."
-    else
-        echo "${host} is down."
-  fi
+    if ping -c 1 -W 1 -w 1 "${host}" &> /dev/null; then
+        echo "${host} is up."
+    fi
 done < "${FILE}"
+
+# we can achieve the same output by using the -sn option in nmap.
+# nmap -sn 172.16.10.0/24 | grep "Nmap scan" | awk -F'report for ' '{print $2}'
