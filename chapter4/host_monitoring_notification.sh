ec206e40c89f57178e21a72667afda44302b1bda
OUBOUSKEN ILYAS <iliasoub15@gmail.com>
2026-07-31 15:03:44 +0100

    Fix typo in variable name for network in host monitoring script

--- chapter4/host_monitoring_notification.sh
+++ chapter4/host_monitoring_notification.sh
@@ -3,14 +3,14 @@
 # Sends a notification upon new host discovery.
 
 KNOW_HOSTS="172-16.10-hosts.txt"
-NETWOK="172.16.10.0/24"
+NETWORK="172.16.10.0/24"
 INTERFACE="br_public"
 FROM_ADDR="example.gmail.com"
 TO_ADDR="exapmle1.gmail.com"
 
 while true; do
     echo "Performing an ARP scanning on ${NETWORK}"
-    sudo arp-scan -x -I ${INTERFACE} ${NETWOK} | while read -r line;
+    sudo arp-scan -x -I ${INTERFACE} ${NETWORK} | while read -r line;
 do
     	host=$(echo "$(line)" | awk 'print $1')
 	if ! grep -q "$(host)" "${KNOW_HOSTS}"; then
