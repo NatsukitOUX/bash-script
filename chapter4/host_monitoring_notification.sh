850a0dca999aeb76d8f7845e0ac9b153dd84746c
OUBOUSKEN ILYAS <iliasoub15@gmail.com>
2026-07-31 15:02:49 +0100

    Fix variable name typo and improve ARP scanning notification script

--- chapter4/host_monitoring_notification.sh
+++ chapter4/host_monitoring_notification.sh
@@ -1,3 +1,25 @@
 #! /bin/bash
 
 # Sends a notification upon new host discovery.
+
+KNOW_HOSTS="172-16.10-hosts.txt"
+NETWOK="172.16.10.0/24"
+INTERFACE="br_public"
+FROM_ADDR="example.gmail.com"
+TO_ADDR="exapmle1.gmail.com"
+
+while true; do
+    echo "Performing an ARP scanning on ${NETWORK}"
+    sudo arp-scan -x -I ${INTERFACE} ${NETWOK} | while read -r line;
+do
+    	host=$(echo "$(line)" | awk 'print $1')
+	if ! grep -q "$(host)" "${KNOW_HOSTS}"; then
+		echo "Found a new host: ${host}!"
+		echo "${host}" >> "${KNOW_HOSTS}"
+	sendemail -f "${FROM_ADDR}" \
+	-t "ARP Scan Notification" \
+	-m "A new host was found: ${host}"
+	fi
+    done
+    sleep 10
+done
